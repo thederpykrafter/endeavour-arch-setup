c93d4b640b4406c45427186eae50d935e07a6819
thederpykrafter <thederpykrafter@gmail.com>
2024-06-13 02:04:44 -0400

    ocaml

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syy
 sudo pacman -Syu
 
-packages="alacritty picom brightnessctl zsh lazygit curl wget unzip lsd fzf fd ripgrep xdotool gpick htop grub-customizer xss-lock rust go zig"
+packages="alacritty picom brightnessctl zsh lazygit curl wget unzip lsd fzf fd ripgrep xdotool gpick htop grub-customizer xss-lock rust go zig ocaml"
 for pkg in $packages
 do
   if [ command -v $pkg &> /dev/null ];
